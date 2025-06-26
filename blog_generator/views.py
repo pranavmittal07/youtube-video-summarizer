5b28d07336c33858c42da1e4e20e5d5296f2b6d3
Pranav Mittal <pranavmittal.in@gmail.com>
2025-06-27 01:26:24 +0530

    undo last commit

--- blog_generator/views.py
+++ blog_generator/views.py
@@ -73,7 +73,7 @@ def generate_blog(request):
 
 def fetch_yt_title(link):
     try:
-        yt = YouTube(link, use_po_token=True)
+        yt = YouTube(link)
         return yt.title
     except VideoUnavailable as e:
         print(f"Error fetching YouTube title: {e}")
@@ -84,7 +84,7 @@ def fetch_yt_title(link):
 
 def download_audio(link):
     try:
-        yt = YouTube(link, use_po_token=True)
+        yt = YouTube(link)
         audio_stream = yt.streams.filter(only_audio=True).first()
 
         if not audio_stream:
