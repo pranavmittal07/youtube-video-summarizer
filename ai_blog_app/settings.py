1519357e8539617b89dd87706d4a1a2ae3bae481
Pranav Mittal <pranavmittal.in@gmail.com>
2025-06-27 00:38:05 +0530

    db changed

--- ai_blog_app/settings.py
+++ ai_blog_app/settings.py
@@ -97,6 +97,9 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
+        'OPTIONS': {
+            'sslmode': os.getenv('DB_SSLMODE', 'require'),
+        },
     }
 }
 
